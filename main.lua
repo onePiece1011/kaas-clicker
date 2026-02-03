058b6b14326423fa22c8fc5737dd21b63ccecca2
JalenV <105585627+JalenPower@users.noreply.github.com>
2026-02-03 10:19:06 +0100

    3 upgrades toegevoegd frenzy gemaakt koop buttons waar je ook echt de upgrades krijgt  met dynamisch text dynamische prijzen
    Toegevoegd:
    Koe + 0.5 per click
    Boer + 10 per 50 clicks
    Kaaswinkel + 2 per seconde
    Als je meerdere upgrades heb krijg je ook meerdere buffs: (buff * koe, boer of kaaswinkel) hangt er van af wat je hebt
    koop knoppen met dynamische text
    koopfuncties met dynamische prijzen math.floor(prijs * 1.5)

--- main.lua
+++ main.lua
@@ -1,59 +1,137 @@
-local count = 0
+local kaas = 0
+local totalclicks = 1
+local koe = 0
+local boer = 0
+local kaaswinkel = 0
+local bonusgiven = false
+local frenzyactive = false
+local koecost = 50
+local boercost = 200
+local kaaswinkelcost = 500
 
 -- Tekst die het nummer laat zien
 local numberText = display.newText({
-    text = "Number: 0",
+    text = "Kaas: 0",
     x = display.contentCenterX,
     y = display.contentCenterY - 50,
     fontSize = 40
 })
 
--- Maximum bereikt tekst (verborgen)
-local maxText = display.newText({
-    text = "Maximum bereikt",
-    x = display.contentCenterX,
-    y = display.contentCenterY + 50,
-    fontSize = 35
+
+local buyKoeButton = display.newRect(display.contentCenterX, display.contentCenterY + 90, 200, 50)
+buyKoeButton:setFillColor(0.4, 0.8, 0.2)
+local buykoetext = display.newText({
+    text = "Koop Koe ( " .. koecost .. " kaas )",
+    x = buyKoeButton.x,
+    y = buyKoeButton.y,
+    fontSize = 20
 })
-maxText.isVisible = false
 
--- Reset knop (verborgen)
-local resetButton = display.newRect(display.contentCenterX, display.contentCenterY + 120, 200, 60)
-resetButton:setFillColor(0.2, 0.6, 1)
-resetButton.isVisible = false
-
-local resetText = display.newText({
-    text = "RESET",
-    x = resetButton.x,
-    y = resetButton.y,
-    fontSize = 30
+local buyBoerButton = display.newRect(display.contentCenterX, display.contentCenterY + 150, 200, 50)
+buyBoerButton:setFillColor(0.4, 0.8, 0.2)
+local buyboertext = display.newText({
+    text = "Koop Boer ( " .. boercost .. " kaas )",
+    x = buyBoerButton.x,
+    y = buyBoerButton.y,
+    fontSize = 20
 })
-resetText.isVisible = false
 
--- Reset functie
-local function resetCounter()
-    count = -1
-    numberText.text = "Number: 1"
-    maxText.isVisible = false
-    resetButton.isVisible = false
-    resetText.isVisible = false
+local buyKaasWinkelButton = display.newRect(display.contentCenterX, display.contentCenterY + 210, 200, 50)
+buyKaasWinkelButton:setFillColor(0.4, 0.8, 0.2)
+local buykaaswinkeltext = display.newText({
+    text = "Koop Kaaswinkel ( " .. kaaswinkelcost .. " kaas )",
+    x = buyKaasWinkelButton.x,
+    y = buyKaasWinkelButton.y,
+    fontSize = 20
+})
+
+local function koopkoe()
+    if kaas >= koecost then
+        koe = koe + 1
+        kaas = kaas - koecost
+        koecost = math.floor(koecost * 1.5)
+        buykoetext.text = "Koop Koe ( " .. koecost .. " kaas ) - " .. koe
+        numberText.text = "Kaas: " .. kaas
+    end
 end
 
-resetButton:addEventListener("tap", resetCounter)
+   local function koopboer()
+    if kaas >= boercost then
+        boer = boer + 1
+        kaas = kaas - boercost
+        boercost = math.floor(boercost * 1.5)
+        buyboertext.text = "Koop Boer ( " .. boercost .. " kaas ) - " .. boer
+        numberText.text = "Kaas: " .. kaas
+    end
+end
+
+local function koopkaaswinkel()
+    if kaas >= kaaswinkelcost then
+        kaaswinkel = kaaswinkel + 1
+        kaas = kaas - kaaswinkelcost
+        kaaswinkelcost = math.floor(kaaswinkelcost * 1.5)
+        buykaaswinkeltext.text = "Koop Kaaswinkel ( " .. kaaswinkelcost .. " kaas ) - " .. kaaswinkel
+        numberText.text = "Kaas: " .. kaas
+    end
+end
+
 
 -- Klik op scherm → +1
 local function onScreenTap()
-    if count < 10 then
-        count = count + 1
-        numberText.text = "Number: " .. count
-    end
+    if kaas < math.huge then
+        kaas = kaas + 1
+        totalclicks = totalclicks + 1
+     
+        -- berekent de bonusen
 
-    -- Als maximum bereikt is
-    if count == 10 then
-        maxText.isVisible = true
-        resetButton.isVisible = true
-        resetText.isVisible = true
+        -- koe bonus
+        if koe >= 1 then
+            kaas = kaas + (0.5 * koe)
+        end
+        
+        -- Frenzy bonus
+        if frenzyactive then
+            kaas = kaas + (4 * (1 + (0.5 * koe)))
+        end
+        
+ -- elke 50 kaas geeft het een 10 click bonus als er een boer is
+        if boer >= 1 then
+            if totalclicks >= 50 then
+                bonusgiven = true
+                if bonusgiven == true then
+                    kaas = kaas + (10 * boer)
+                    totalclicks = totalclicks - 49
+                end
+            end
+        end
+        
+        numberText.text = "Kaas: " .. kaas
+    end
+end 
+
+Runtime:addEventListener("tap", onScreenTap)
+buyKoeButton:addEventListener("tap", koopkoe)
+buyBoerButton:addEventListener("tap", koopboer)
+buyKaasWinkelButton:addEventListener("tap", koopkaaswinkel)
+
+local function kaaswinkelProduction()
+    if kaaswinkel >= 1 then
+        kaas = kaas + (2 * kaaswinkel)
+        numberText.text = "Kaas: " .. kaas
     end
 end
 
-Runtime:addEventListener("tap", onScreenTap)
+local function startFrenzyClicker()
+    frenzyactive = true
+    FrenzyNotifier = display.newText {
+        text = "Frenzy Actief!",
+        x = display.contentCenterX,
+        y = display.contentCenterY - 100,
+        fontSize = 50
+    }
+    timer.performWithDelay(5000, function() frenzyactive = false end, 1)
+    timer.performWithDelay(5000, function() FrenzyNotifier:removeSelf() end, 1)
+end
+
+timer.performWithDelay(1000, kaaswinkelProduction, 0)
+timer.performWithDelay(30000, startFrenzyClicker, 0)
